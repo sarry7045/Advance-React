58aab3d683950791df7d7b2e11daab26b4af9ecd
Suraj Anil Yadav <76764745+sarry7045@users.noreply.github.com>
2022-06-22 12:37:50 +0530

    Changes

---
+++ src/Components/JavaScriptTopics/TrickyQuestions.jsx
@@ -0,0 +1,39 @@
+import { ar } from "date-fns/locale";
+import React from "react";
+
+const TrickyQuestions = () => {
+  // console.log(2 +"2") - 22
+  // console.log(2-"2") - 0
+
+  // let nums = [1, 2, , 2, 3]
+  // remove duplicate value
+  // const removeDupli = [...new Set(nums)]
+
+  // console.log(5 < 6 < 7);  true
+  // console.log(7 > 6 > 5); false
+
+  // const arry = [1, 2, 15, 30, 5, 45, 7]
+  // arry.sort()
+  // console.log(arry.sort(() => {
+  //     return a<b
+  // }))
+
+  // const x = [1, 2, 3] + [4, 5, 6]
+  // console.log(x) - 1,2,34,5,6,7
+  // const x = [...[1, 2, 3], ...[4, 5, 6]]
+  // console.log(x) - 1,2,3,4,5,6,7
+
+  // const arr = [1, 1, 0, 0, 0, 0, 1, 1]
+  // const findOne = arr.filter((val) => {
+  //   return val === 1
+  // })
+  // console.log(findOne.length)
+
+  // console.log("1" + 1)
+  // console.log(1 + "1")
+  // both prints 11
+
+  return <div>TrickyQuestions</div>;
+};
+
+export default TrickyQuestions;
---
+++ src/Components/ReactLogics/useCallback/CBPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CBPage = ({example}) => {
+  console.log("CBpage");
+  return <div></div>;
+};
+
+export default CBPage;
